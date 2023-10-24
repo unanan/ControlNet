81437f743e73b3ace6a7e9b812639522bd5a1763
unaG <veraguo11@163.com>
2023-10-24 14:14:37 +0800

    Update gradio_canny2image.py

--- gradio_canny2image.py
+++ gradio_canny2image.py
@@ -18,7 +18,7 @@ from cldm.ddim_hacked import DDIMSampler
 apply_canny = CannyDetector()
 
 model = create_model('./models/cldm_v15.yaml').cpu()
-model.load_state_dict(load_state_dict('./models/control_sd15_canny.pth', location='cuda'))
+model.load_state_dict(load_state_dict('/media/weights/ControlNet/pretrained/control_sd15_canny.pth', location='cuda'))
 model = model.cuda()
 ddim_sampler = DDIMSampler(model)
 
@@ -94,4 +94,4 @@ with block:
     run_button.click(fn=process, inputs=ips, outputs=[result_gallery])
 
 
-block.launch(server_name='0.0.0.0')
+block.launch(server_name='0.0.0.0', )
